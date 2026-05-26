0fe9cd58c6ed8e51fbfb45dc569e5349e980983a
Sakthi002-D <sakthivel.d002@gmail.com>
2026-05-26 09:04:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,68 @@
-//your JS code here. If required.
+let player1 = "";
+let player2 = "";
+let currentPlayer = "X";
+
+const submitBtn = document.getElementById("submit");
+const board = document.getElementById("board");
+const message = document.querySelector(".message");
+const cells = document.querySelectorAll(".cell");
+
+submitBtn.addEventListener("click", () => {
+
+    player1 = document.getElementById("player-1").value;
+    player2 = document.getElementById("player-2").value;
+
+    message.innerText = `${player1}, you're up`;
+});
+
+cells.forEach((cell) => {
+
+    cell.addEventListener("click", () => {
+
+        if (cell.innerText !== "") {
+            return;
+        }
+
+        cell.innerText = currentPlayer;
+
+        checkWinner();
+
+        if (currentPlayer === "X") {
+            currentPlayer = "O";
+            message.innerText = `${player2}, you're up`;
+        } else {
+            currentPlayer = "X";
+            message.innerText = `${player1}, you're up`;
+        }
+
+    });
+
+});
+
+function checkWinner() {
+
+    const winPatterns = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ];
+
+    for (let pattern of winPatterns) {
+
+        let a = cells[pattern[0]].innerText;
+        let b = cells[pattern[1]].innerText;
+        let c = cells[pattern[2]].innerText;
+
+        if (a !== "" && a === b && b === c) {
+
+            let winner = a === "X" ? player1 : player2;
+
+            message.innerText = `${winner}, congratulations you won!`;
+        }
+    }
+}
